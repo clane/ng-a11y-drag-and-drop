9a6bb40802ff952e08a77fffa3eb11bce9f8d19e
Christopher Lane <clane@Christophers-MacBook-Pro.local>
2019-03-09 17:03:14 -0800

    arrow keys now moving object

--- App/src/app/app.component.ts
+++ App/src/app/app.component.ts
@@ -8,25 +8,61 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'A11y Drag and Drop';
 
- allowDrop(event) {
+  allowDrop(event) {
     event.preventDefault();
   }
-  
- drag(event) {
+
+  drag(event) {
     //event.dataTransfer.setData("text", event.target.id);
-    console.log('in drag'); 
-    console.log(event); 
+    console.log('in drag');
+    console.log(event);
   }
-  
- drop(event) {
+
+  drop(event) {
     event.preventDefault();
     var data = event.dataTransfer.getData("text");
     event.target.appendChild(document.getElementById(data));
   }
 
-  getKeyAndMove($event){
-    console.log('in getKeyAndMove'); 
-    console.log(event); 
+  getKeyAndMove($event) {
+    console.log('in getKeyAndMove');
+    console.log(event);
+    console.log(event.keyCode);
+
+    var keyCode = event.keyCode;
+    switch (keyCode) {
+      case 37: //left arrow key
+        this.moveLeft();
+        break;
+      case 38: //Up arrow key
+        this.moveUp();
+        break;
+      case 39: //right arrow key
+        this.moveRight();
+        break;
+      case 40: //down arrow key
+        this.moveDown();
+        break;
+    }
+  }
+
+  moveLeft(){
+    console.log('move left');
+    
+  }
+
+  moveUp(){
+    console.log('move up');
+
+  }
+
+  moveRight(){
+    console.log('move right');
+
+  }
+
+  moveDown() {
+    console.log('move down');
 
   }
 
