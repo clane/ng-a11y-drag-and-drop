f2cf2e2e7d7b31e7228736954a441d12cc0cca03
Chris Lane <chlane@chlane-a01.vmware.com>
2019-03-20 11:35:09 -0700

    now have buttons working to move in each direction and moved more announcements into the live region

--- App/src/app/app.component.ts
+++ App/src/app/app.component.ts
@@ -49,68 +49,70 @@ export class AppComponent {
     }
   
     var keyCode = $event.keyCode;
-    var message;
 
     switch (keyCode) {
       case 37: //left arrow key
-        message = "moving left" + " Top:" + dragObjRect.top + " Left:" + dragObjRect.left;
-        this.announceDragDirection(message); 
         this.moveLeft();
         break;
       case 38: //Up arrow key
-        message = "moving up" + " Top:" + dragObjRect.top + " Left:" + dragObjRect.left;
-        this.announceDragDirection(message); 
         this.moveUp();
         break;
       case 39: //right arrow key
-        message = "moving right" + " Top:" + dragObjRect.top + " Left:" + dragObjRect.left;
-        this.announceDragDirection(message); 
         this.moveRight();
         break;
-      case 40: //down arrow key
-        message = "moving down" + " Top:" + dragObjRect.top + " Left:" + dragObjRect.left;
-        this.announceDragDirection(message);  
+      case 40: //down arrow key 
         this.moveDown();
         break;
     }
 
     this.renderer.setStyle(this.elementToDrag.nativeElement, "left", this.left + 'px');
     this.renderer.setStyle(this.elementToDrag.nativeElement, "top", this.top + 'px');
-
- 
-    
   }
 
   moveLeft() {
+    let dragObjRect = this.elementToDrag.nativeElement.getBoundingClientRect();
     this.left = this.left - 5;
+    let message = "moving left" + " Top:" + dragObjRect.top + " Left:" + dragObjRect.left;
+    this.updateLiveRegion(message); 
   }
 
   moveUp() {
+    let dragObjRect = this.elementToDrag.nativeElement.getBoundingClientRect();
     this.top = this.top - 5;
+    let message = "moving up" + " Top:" + dragObjRect.top + " Left:" + dragObjRect.left;
+    this.updateLiveRegion(message); 
   }
 
   moveRight() {
+    let dragObjRect = this.elementToDrag.nativeElement.getBoundingClientRect();
     this.left = this.left + 5;
+    let message = "moving right" + " Top:" + dragObjRect.top + " Left:" + dragObjRect.left;
+    this.updateLiveRegion(message); 
   }
 
   moveDown() {
+    let dragObjRect = this.elementToDrag.nativeElement.getBoundingClientRect();
     this.top = this.top + 5;
+    let message = "moving down" + " Top:" + dragObjRect.top + " Left:" + dragObjRect.left;
+    this.updateLiveRegion(message); 
+    
+     
   }
 
   showKeyboardDragInstructions(){
-    alert('Use the arrow keys to drag this item to the dropzone');
+    let message = 'Use the arrow keys to drag this item to the dropzone';
+    this.updateLiveRegion(message);
   }
 
   moveObject(){
     this.renderer.appendChild(this.placeToDrop.nativeElement, this.elementToDrag.nativeElement);
     this.renderer.setStyle(this.elementToDrag.nativeElement, "position", 'static');
-    
+    this.updateLiveRegion("The draggable object has been moved to the drop zone");
   }
 
-  announceDragDirection(message){
+  updateLiveRegion(message){
     var text = this.renderer.createText(message);
     this.renderer.appendChild(this.liveRegionToUpdate.nativeElement, text);
-  
   }
 
 }
